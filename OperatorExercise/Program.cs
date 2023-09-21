476b1e6c02e1f91f8d5960f7ba9cf293097f3238
Sammie Gay <smylezstudioz@gmail.com>
2023-09-21 14:44:28 -0500

    Both Exercises Completed.

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace OperatorExercise
+namespace OperatorExercise;
+
+public class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        var a = 17;
+        var b = 4;
+        var quotient = a / b;
+        var remainder = a % b;
+
+        Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+        Console.WriteLine("Please enter the radius of your circle.");
+        var userInput = Console.ReadLine();
+
+        var radius = double.Parse(userInput);
+
+        Console.WriteLine(AreaOfCircle(radius));
+
     }
+
+    public static double AreaOfCircle(double radius)
+    {
+        var area = Math.PI * Math.Pow(radius, 2);
+
+        return area;
+
+    }
+    
 }
